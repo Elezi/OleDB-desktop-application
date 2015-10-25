32f4c9c86bb1ca5441a5e57e7b77b648094e2646
Nuri Osmani <osmani.nuri@gmail.com>
2015-10-25 22:56:40 +0100

    Unit Test Project created

---
+++ John.SocialClub/MosqueCommunityTest/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MosqueCommunityTest
+{
+    [TestClass]
+    public class HelloWorld
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+        }
+    }
+}
